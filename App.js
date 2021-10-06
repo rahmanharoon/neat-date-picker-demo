14e646005afa327013caf8ca51d91ee32f789c28
rahmanharoon <rahmanharoon128@gmail.com>
2021-10-06 21:59:42 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,50 @@
+
+import React, { useState } from 'react'
+import { StyleSheet, View, Button } from 'react-native'
+import DatePicker from 'react-native-neat-date-picker'
+
+const App = () => {
+
+  const [showDatePicker, setShowDatePicker] = useState(false)
+
+  const openDatePicker = () => {
+    setShowDatePicker(true)
+  }
+
+  const onCancel = () => {
+    // You should close the modal in here
+    setShowDatePicker(false)
+  }
+
+  const onConfirm = (date) => {
+    // You should close the modal in here
+    setShowDatePicker(false)
+
+    // The parameter 'date' is a Date object so that you can use any Date prototype method.
+    console.log(date.getDate())
+  }
+
+  return (
+    <View style={styles.container}>
+      <Button title={'open'} onPress={openDatePicker} />
+      <DatePicker
+        isVisible={showDatePicker}
+        mode={'single'}
+        onCancel={onCancel}
+        onConfirm={onConfirm}
+      />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
+
